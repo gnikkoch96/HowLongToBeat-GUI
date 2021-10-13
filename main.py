c9b7b3c9da5872a0468b70f30383d8816f1e3835
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2021-10-13 09:08:38 -0700

    game_imgs folder is cleanedup after program ends

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import dearpygui.dearpygui as dpg
+import os
 from hltbgui import HLTBGUI
 
-# static
+# vars
 VIEWPORT_HEIGHT = 1000
 VIEWPORT_WIDTH = 1000
-
+GAME_IMG_DIR = "resources/game_imgs"
 
 def create_win():
     # dpg viewport
@@ -21,13 +22,19 @@ def create_win():
 
     dpg.start_dearpygui()
 
+
 def create_dpg_themes():
     with dpg.theme(default_theme=True):
         dpg.add_theme_color(dpg.mvThemeCol_WindowBg, (156, 209, 28), category=dpg.mvThemeCat_Core)
 
+
+def cleanup_game_folder():
+    # delete all of the images from the game_imgs folder
+    for file in os.listdir(GAME_IMG_DIR):
+        os.remove(os.path.join(GAME_IMG_DIR, file))
+
+
 if __name__ == '__main__':
     # dpg.show_style_editor()
     create_win()
-
-    # delete the game_imgs folder
-    
+    cleanup_game_folder()
